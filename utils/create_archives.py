9fa54c695b71df27236d4e82afea68a7bf28781f
Roy Tang <roytang@gmail.com>
2019-09-29 18:26:08 +0800

    Fix archive generation script to account for all post types

--- utils/create_archives.py
+++ utils/create_archives.py
@@ -1,33 +1,29 @@
 from pathlib import Path
+from importers.utils import loadurlmap, contentdir
+from datetime import datetime
 
 year_template = '---\ntitle: "Yearly Archives for %s"\ndate: %s-01-01 00:00:00\n---'
 month_template = '---\ntitle: "Monthly Archives for %s"\ndate: %s-%s-01 00:00:00\n---'
 
 def create_archives():
-    # assumes cwd is the blog root
-    cwd = Path.cwd() 
-    # navigate to ./content/post
-    p = cwd / "content" / "post"
-    if p.name != "post":
-        print("Could not find post folder")
-        return
-    # immediately under the posts are the year folders
-    for year in p.iterdir():
-        # outer loop generates the year templates
-        if year.is_dir():
-            yearcontent = year_template % (year.name, year.name)
-            target_file = cwd / "content" / "archy" / (year.name + ".md")
-            if True: #not target_file.exists():
+    urlmap = loadurlmap()
+    for u in urlmap:
+        info = urlmap[u]
+        if info.get("source_path", "page\\").startswith("page\\"):
+            continue
+        if 'date' in info:
+            d = datetime.strptime(info["date"], "%Y-%m-%d")
+            year = d.strftime("%Y")
+            target_file = contentdir / "archy" / (year + ".md")
+            if not target_file.exists():
                 with target_file.open("w") as f:
+                    yearcontent = year_template % (year, year)
                     f.write(yearcontent)
-            for month in year.iterdir():
-                # inner loop generates the month templates
-                if month.is_dir():
-                    monthcontent = month_template % (month.name + ' ' + year.name, year.name, month.name)
-                    target_file = cwd / "content" / "archm" / (year.name + "-" + month.name + ".md")
-                    if True: #not target_file.exists():
-                        with target_file.open("w") as f:
-                            f.write(monthcontent)
-
+            month = d.strftime("%m")
+            target_file = contentdir / "archm" / (year + "-" + month + ".md")
+            if not target_file.exists():
+                with target_file.open("w") as f:
+                    monthcontent = month_template % (month + ' ' + year, year, month)
+                    f.write(monthcontent)
 
 create_archives()
