717dcce51fbc6e36ba08f3e4787b9556d49a3e87
Roy Tang <roytang@gmail.com>
2020-01-29 09:00:45 +0800

    Oops, should append to file instead

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-date > log.txt
-hugo version > log.txt
-hugo --gc --minify --templateMetrics > log.txt
+date >> log.txt
+hugo version >> log.txt
+hugo --gc --minify --templateMetrics >> log.txt
 mkdir public/build
 mv log.txt public/build/log.txt
