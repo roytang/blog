1f9715462094146d55f2d17056e6acc3fdcbcff2
Roy Tang <roytang@gmail.com>
2020-02-06 20:24:35 +0800

    temporarily remove minification due to https://github.com/gohugoio/hugo/issues/6472#issuecomment-559238453

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 date >> log.txt
 hugo version >> log.txt
-hugo --gc --minify --templateMetrics >> log.txt
+hugo --gc --templateMetrics >> log.txt
 mkdir public/build
 mv log.txt public/build/log.txt
