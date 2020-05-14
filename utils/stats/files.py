7e0d18b29e8316cc44b9658773d75b6c5a4893f2
Roy Tang <4228+roytang@users.noreply.github.com>
2020-05-14 23:36:11 +0800

    file stats

---
+++ utils/stats/files.py
@@ -0,0 +1,26 @@
+# generate that stats json file for all site/blog items
+
+from pathlib import Path
+import os
+import json
+from datetime import datetime
+
+def count_files():
+    count = 0
+    cwd = Path.cwd() 
+    # navigate to ./content/posts
+    stats_by_suffix = {}
+    p = cwd / "content" # / "notes" # / "2010" / "08"
+    for afile in p.glob("**/*.*"):
+        if not afile.is_dir():
+            suffix = afile.suffix
+            size = afile.stat().st_size / 1024 / 1024
+            if afile.suffix not in stats_by_suffix:
+                stats_by_suffix[suffix] = { "count": 1, "size": size }
+            else:
+                stats_by_suffix[suffix]["count"] = stats_by_suffix[suffix]["count"] + 1
+                stats_by_suffix[suffix]["size"] = stats_by_suffix[suffix]["size"] + size
+
+    print(json.dumps(stats_by_suffix, indent=2))
+
+count_files()
