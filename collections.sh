79618161b48975392415ea0db534e0236ba5258c
Roy Tang <roytang@gmail.com>
2024-06-17 14:55:58 +0800

    collections_root

--- collections.sh
+++ collections.sh
@@ -1,1 +1,2 @@
+hugo --contentDir collections_root --config collections.toml --destination /var/www/collections/ 
 hugo --contentDir collections/lyrics --config collections/lyrics/lyrics.toml,collections.toml --destination /var/www/collections/lyrics
