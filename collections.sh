930ddb319410c67a9078e1869da478fbeb3f0509
Roy Tang <roytang@gmail.com>
2024-06-25 14:52:50 +0800

    quotes first pass

--- collections.sh
+++ collections.sh
@@ -1,2 +1,3 @@
 hugo --contentDir collections_root --config collections.toml --destination /var/www/collections/ 
 hugo --contentDir collections/lyrics --config collections.toml,collections/lyrics/lyrics.toml --destination /var/www/collections/lyrics
+hugo --contentDir collections/quotes --config collections.toml,collections/quotes/quotes.toml --destination /var/www/collections/quotes
