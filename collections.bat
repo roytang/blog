8e3fb1bb5a34bfa4c709a71f14ede382d533959e
Roy Tang <roytang@gmail.com>
2024-06-26 17:09:00 +0800

    collection embeds!!

---
+++ collections.bat
@@ -0,0 +1,3 @@
+hugo --contentDir collections_root --config collections.toml --destination public
+hugo --contentDir collections/lyrics --config collections.toml,collections/lyrics/lyrics.toml --destination public/lyrics
+hugo --contentDir collections/quotes --config collections.toml,collections/quotes/quotes.toml --destination public/quotes
--- content/post/2021/09/weeknotes-09-19/index.md
+++ content/post/2021/09/weeknotes-09-19/index.md
@@ -20,7 +20,7 @@ Just another week.
 
 ### A quote:
 
-> "Wealth consists not in having great possessions, but in having few wants." – [Epictetus](/collections/quotes/511527699391594496/)
+{{% collections-embed quotes 511527699391594496 %}}
 
 ### The world:
 
