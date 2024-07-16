f4495964bb2c1e4a6e770b3b4b750fb55c2add80
Roy Tang <roytang@gmail.com>
2024-07-16 16:17:34 +0800

    collections/comics first pass

--- collections.sh
+++ collections.sh
@@ -1,3 +1,4 @@
 hugo --contentDir collections_root --config collections.toml --destination /var/www/collections/ 
 hugo --contentDir collections/lyrics --config collections.toml,collections/lyrics/lyrics.toml --destination /var/www/collections/lyrics
 hugo --contentDir collections/quotes --config collections.toml,collections/quotes/quotes.toml --destination /var/www/collections/quotes
+hugo --contentDir collections/comics --config collections.toml,collections/quotes/comics.toml --destination /var/www/collections/comics
