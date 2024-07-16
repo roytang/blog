57f4d56ccce8d4de1b6ac1c1997b2fae8bb80970
Roy Tang <roytang@gmail.com>
2024-07-16 16:22:09 +0800

    oops

--- collections.bat
+++ collections.bat
@@ -1,3 +1,4 @@
 hugo --contentDir collections_root --config collections.toml --destination public
 hugo --contentDir collections/lyrics --config collections.toml,collections/lyrics/lyrics.toml --destination public/lyrics
 hugo --contentDir collections/quotes --config collections.toml,collections/quotes/quotes.toml --destination public/quotes
+hugo --contentDir collections/comics --config collections.toml,collections/comics/comics.toml --destination public/comics
