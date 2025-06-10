46f52cea60435c6186be127fc3f1ac604cfa42f2
Roy Tang <roytang@gmail.com>
2025-06-10 16:37:37 +0800

    blorp

--- collections.sh
+++ collections.sh
@@ -11,4 +11,4 @@ hugo --contentDir collections/threads --config collections.toml,collections/thre
 hugo --contentDir collections/surveys --config collections.toml,collections/surveys/surveys.toml --destination /var/www/collections/surveys
 hugo --contentDir collections/jokes --config collections.toml,collections/jokes/jokes.toml --destination /var/www/collections/jokes
 hugo --contentDir collections/miiverse --config collections.toml,collections/miiverse/miiverse.toml --destination /var/www/collections/miiverse
-hugo --contentDir collections/quiznights --config collections.toml,collections/quiznights/quiznightsocations.toml --destination /var/www/collections/quiznights
+hugo --contentDir collections/quiznights --config collections.toml,collections/quiznights/quiznights.toml --destination /var/www/collections/quiznights
