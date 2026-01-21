2758e8aec8f33131d99fe320dacb0f8e98f6477a
Roy Tang <roytang@gmail.com>
2026-01-21 09:47:39 +0800

    add collections batch file for mint

---
+++ collectionsmint.sh
@@ -0,0 +1,15 @@
+hugo --contentDir collections_root --config collections.toml --destination /var/www/html/collections/ 
+hugo --contentDir collections/lyrics --config collections.toml,collections/lyrics/lyrics.toml --destination /var/www/html/collections/lyrics
+hugo --contentDir collections/quotes --config collections.toml,collections/quotes/quotes.toml --destination /var/www/html/collections/quotes
+hugo --contentDir collections/comics --config collections.toml,collections/comics/comics.toml --destination /var/www/html/collections/comics
+hugo --contentDir collections/albums --config collections.toml,collections/albums/albums.toml --destination /var/www/html/collections/albums
+hugo --contentDir collections/stackexchange --config collections.toml,collections/stackexchange/stackexchange.toml --destination /var/www/html/collections/stackexchange
+hugo --contentDir collections/webcomics --config collections.toml,collections/webcomics/webcomics.toml --destination /var/www/html/collections/webcomics
+hugo --contentDir collections/quora --config collections.toml,collections/quora/quora.toml --destination /var/www/html/collections/quora
+hugo --contentDir collections/sketchbook --config collections.toml,collections/sketchbook/sketchbook.toml --destination /var/www/html/collections/sketchbook
+hugo --contentDir collections/threads --config collections.toml,collections/threads/threads.toml --destination /var/www/html/collections/threads
+hugo --contentDir collections/surveys --config collections.toml,collections/surveys/surveys.toml --destination /var/www/html/collections/surveys
+hugo --contentDir collections/jokes --config collections.toml,collections/jokes/jokes.toml --destination /var/www/html/collections/jokes
+hugo --contentDir collections/miiverse --config collections.toml,collections/miiverse/miiverse.toml --destination /var/www/html/collections/miiverse
+hugo --contentDir collections/quiznights --config collections.toml,collections/quiznights/quiznights.toml --destination /var/www/html/collections/quiznights
+hugo --contentDir collections/fiction --config collections.toml,collections/fiction/fiction.toml --destination /var/www/html/collections/fiction
